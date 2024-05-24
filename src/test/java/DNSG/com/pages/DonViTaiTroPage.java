5f7410cf3834d0c94327fe21f477913b563616bb
QuangDK1 <pnquang.284@gmail.com>
2024-05-24 17:03:11 +0700

    them_xoa_sua_dvtt

--- src/test/java/DNSG/com/pages/DonViTaiTroPage.java
+++ src/test/java/DNSG/com/pages/DonViTaiTroPage.java
@@ -1,6 +1,5 @@
 package DNSG.com.pages;
 
-import DNSG.com.pages.customers.AddDonViTaiTroPage;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import quangtester.com.keywords.WebUI;
@@ -9,49 +8,50 @@ public class DonViTaiTroPage {
     private String URL = "https://saigon-business.erp.meu-solutions.com/admin/partner";
     private String PAGE_TEST = "Đơn vị tài trợ";
     By headerPageDonViTaiTroAdmin = By.xpath("//h1[contains(text(),'Đơn vị tài trợ')]");
+    By clickpagedonvitaitroadmin = By.xpath("//div/div/ul/div[4]/div");
     By buttonAddDonViTaiTro = By.xpath("//div/div/div[1]/div/button");
     By buttonXoaDonViTaiTro = By.xpath("//div/div/div[1]/div[5]/div/button[2]");
-    By buttonEditDonViTaiTro = By.xpath("//div/div/div[1]/div[5]/div/button[1]");
+    By buttonEditDonViTaiTro = By.xpath("//div/div[3]/div[5]/div/button[1]");
+    By Logo = By.xpath("//div[2]/div/div[1]/label");
+    By Tendonvi = By.xpath("//div/div[2]/div/input");
+    By Linkdonvi = By.xpath("//div/div[3]/div/input");
+    By buttonLuuDonvi = By.xpath("//div/div[3]/button[2]");
+    By doituong = By.xpath("//div[2]/div/div[1]/div[2]/div/div/div[3]");
+    By popupXoa = By.xpath("//div/div[2]/button[2]");
+    By buttonLuuEdit = By.xpath("//div/div[3]/button[2]");
+
+    //page add mới đơn vị
+    private String PAGE_URL_DVTTADMIN = "https://saigon-business.erp.meu-solutions.com/admin/partner";
+    private String PAGE_TEXT_THEMDVTT = "Thêm mới Đơn vị tài trợ";
 
 
 
-    public void verifyDonViTaiTroPageAdmin(){
-        boolean donvitaitro = WebUI.getCurrentUrl().equals("https://saigon-business.erp.meu-solutions.com/admin/partner");
-        Assert.assertTrue(donvitaitro,"Fail");
-    }
-
-    public AddDonViTaiTroPage clickNewThemDonViTaiTro(){
+    public void ThemMoiDonViTaiTro(String Ten_Don_Vi , String Link_Don_Vi){
+        WebUI.clickElement(clickpagedonvitaitroadmin);
         WebUI.clickElement(buttonAddDonViTaiTro);
-        return new AddDonViTaiTroPage();
+        WebUI.setText(Tendonvi,Ten_Don_Vi);
+        WebUI.setText(Linkdonvi,Link_Don_Vi);
+        WebUI.clickElement(buttonLuuDonvi);
+        WebUI.sleep(1.5);
     }
 
 
+    public void XoaDonViTaiTro(){
+        WebUI.clickElement(clickpagedonvitaitroadmin);
+        WebUI.clickElement(doituong);
+        WebUI.clickElement(buttonXoaDonViTaiTro);
+        WebUI.clickElement(popupXoa);
+        WebUI.sleep(2);
+
+    }
+    public void EditDonViTaiTro(){
+        WebUI.clickElement(clickpagedonvitaitroadmin);
+        WebUI.clickElement(doituong);
+        WebUI.clickElement(buttonEditDonViTaiTro);
+        
+        WebUI.clickElement(buttonLuuEdit);
+        WebUI.sleep(2);
 
 
-
-//    public void ThemMoiDonViTaiTro (){
-//
-//        WebUI.clickElement(buttonAddDonViTaiTro);
-//    }
-
-//    public void ChinhSuaDonViTaiTro(){
-//
-//    }
-//
-//    public void XoaDonViTaiTro(){
-//
-//    }
-//
-//
-//    public void verifyThaoTacThemDonViTaiTroThanhCong(){
-//
-//    }
-//
-//
-//    public void verifyThaoChinhSuaDonViTaiTroThanhCong(){
-//
-//    }
-//    public void verifyXoaDonViTaiTro(){
-//
-//    }
+    }
 }
--- src/test/java/DNSG/com/testcases/DonViTaiTroTest.java
+++ src/test/java/DNSG/com/testcases/DonViTaiTroTest.java
@@ -3,40 +3,45 @@ package DNSG.com.testcases;
 import DNSG.com.common.BaseTest;
 import DNSG.com.pages.DonViTaiTroPage;
 import DNSG.com.pages.LoginPage;
-import DNSG.com.pages.customers.AddDonViTaiTroPage;
+
+import com.google.j2objc.annotations.Weak;
 import org.testng.annotations.Test;
+import quangtester.com.keywords.WebUI;
 
 public class DonViTaiTroTest extends BaseTest {
     LoginPage loginPage;
     DonViTaiTroPage donViTaiTroPage;
-    AddDonViTaiTroPage addDonViTaiTroPage;
-
-
-    @Test(priority = 0)
-    public void OpenPageDonViTaiTro(){
-        loginPage = new LoginPage();
-        addDonViTaiTroPage = new AddDonViTaiTroPage();
-        donViTaiTroPage = new DonViTaiTroPage();
-        loginPage.LoginThanhCong("admin@meu-solutions.com","123456");
-        donViTaiTroPage.verifyDonViTaiTroPageAdmin();
-        addDonViTaiTroPage.AddNewDataDonViTaiTro("NhaTrang","https://github.com/QuangDK1");
 
 
 
-    }
+
     @Test(priority = 1)
     public void ThemMoiDonViTaiTroThanhCong(){
+        loginPage = new LoginPage();
+        donViTaiTroPage = new DonViTaiTroPage();
+        loginPage.LoginThanhCong("admin@meu-solutions.com","123456");
+        donViTaiTroPage.ThemMoiDonViTaiTro("NhaTrang","https://github.com/QuangDK1/ProjectAutomation_DNSG");
+
 
     }
 
 
     @Test(priority = 2)
     public void XoaDonViTaiTro(){
+        loginPage = new LoginPage();
+        donViTaiTroPage = new DonViTaiTroPage();
+        loginPage.LoginThanhCong("admin@meu-solutions.com","123456");
+        donViTaiTroPage.XoaDonViTaiTro();
+
 
     }
 
     @Test(priority = 3)
     public void ChinhSuaDonViTaiTroThanhCong(){
+        loginPage = new LoginPage();
+        donViTaiTroPage = new DonViTaiTroPage();
+        loginPage.LoginThanhCong("admin@meu-solutions.com","123456");
+        donViTaiTroPage.EditDonViTaiTro();
 
     }
 }
